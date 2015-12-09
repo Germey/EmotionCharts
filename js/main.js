c213962b808bd5a7afeba127d4f409469af49ac7
Qingcai <1016903103@qq.com>
2015-12-09 20:35:59 +0800

    format json

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
-$(function () {
-    var chart2 = {
+
+function addRadioButton(key) {
+    var label = $('<label></label>').addClass('radio').html('<div class="val">'+ key + '</div>');
+    $('<span class="icons"><span class="icon-unchecked">' +
+        '</span><span class="icon-checked"></span></span>').prependTo(label);
+    $('<input>').attr({
+        'name': 'optionsRadios2',
+        'type': 'radio',
+        'data-toggle': 'radio',
+        'required': 'required',
+        'class': 'custom-radio'
+    }).prependTo(label);
+    label.appendTo('#options .form-group');
+}
+
+function getChartType(kind) {
+    var chart_type = {
+        "直方图" : 'column',
+    }
+    return chart_type[kind];
+}
+
+function getChart(type, title, category, data) {
+    return {
         chart: {
-            type: 'column'
+            type: getChartType(type)
         },
         title: {
-            text: 'Monthly Average Rainfall'
+            text: title,
         },
         xAxis: {
-            categories: [
-                '男',
-                '女',
-            ]
+            categories: category
         },
         yAxis: {
             min: 0,
@@ -20,7 +39,7 @@ $(function () {
         },
         series: [{
             name: '人数',
-            data: [6,35]
+            data: data,
         }],
         exporting: {
             enabled: false
@@ -30,11 +49,40 @@ $(function () {
                 color: '#16a085'
             }
         },
-    };
-    $('#chart2-content').highcharts(chart2);
+        credits: {
+            enabled: false
+        },
+    }
+}
+
+$(function () {
     $.getJSON('json/format.json', function(data) {
         $.each(data, function(key, value) {
-            alert(key);
-        })
+            addRadioButton(key);
+        });
+        $('#options .radio input').on('click', function() {
+            button_key = $(this).siblings('.val').text();
+            $.each(data, function(key, value) {
+                if (key == button_key) {
+                    chart_datas = new Array();
+                    $('#text').html('');
+                    $.each(value, function(index, content) {
+                        $('<div></div>').text('TIP' + (index + 1) + '：' + content.tip).addClass('item show-none').attr('for', index).appendTo($('#text'));
+                        chart_data = getChart(content.table.type, content.table.title, content.table.category, content.table.data);
+                        chart_datas.push(chart_data);
+                        console.log(content.table.name);
+                        $('#chart2-content').highcharts(chart_data);
+                    });
+                    $('#text .item').on('click', function() {
+                        $('#text .item').removeClass('active');
+                        $(this).addClass('active');
+                        $('#chart2-content').highcharts(chart_datas[$(this).attr('for')]);
+                    });
+                    $('#text .item').first().trigger('click');
+                }
+            });
+            $('#text .item').fadeIn();
+        });
+        $('#options .radio input').first().trigger('click');
     });
 });
